b8fb4661ca1bd6973ce58363423331c34f9db5da
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-04 09:19:16 -0400

    return 404 not found error

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ let fs = require('fs')
 let server = http.createServer()
 server.on('request', (request, response) => {
     fs.readFile('index.html', (err, data) => {
-        if (err) throw err
-        response.writeHead(200, {'Content-type': 'text/html;'})
-        response.end(data)
+        if (err) {
+            response.writeHead(404)
+            response.end("file not found")
+        } else {
+            response.writeHead(200, {'Content-type': 'text/html;'})
+            response.end(data)
+        }
     })
 })
 server.listen(80)
