c3165369404610410ffde6155d28ff85309a8b2e
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-04 09:00:33 -0400

    first server without close request

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log('hello')
+
+var http = require('http')
+
+let server = http.createServer()
+server.on('request', (request, response) => {
+    console.log('Hello')
+})
+server.listen(80)
