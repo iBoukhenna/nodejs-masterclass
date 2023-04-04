49b2df3ff163ef38176bad0fed1dca33a615d4fe
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-04 09:51:29 -0400

    server async with emitter

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const EventEmitter = require('events')
-let myListner = new EventEmitter()
-myListner.once('jump', function (a, b) {
-    console.log('I jumped', a, b)
-})
 
-myListner.emit('jump', 10, 20)
-myListner.emit('jump')
-myListner.emit('jump', 12)
+let http = require('http')
+
+let App = {
+    start: function (port) {
+        let myEmitter = new EventEmitter()
+        let server = http.createServer((request, response) => {
+            response.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
+            if (request.url === '/') {
+                myEmitter.emit('root', response)
+            }
+            response.end()
+        }).listen(port)
+        return myEmitter
+    }
+}
+
+let app = App.start(80)
+app.on('root', function (response) {
+    response.write('I\'m in root')
+})
