4c3c7c3e552ca858107b61655f867b02d55b9cd5
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-04 09:05:09 -0400

    syntax

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 
 var http = require('http')
 
-let server = http.createServer()
-server.on('request', (request, response) => {
-    response.writeHead(200)
+let server = http.createServer((request, response) => {
+    response.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
     response.end('Hello')
-})
-server.listen(80)
+}).listen(80)
