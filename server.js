a03a8dc955dd4fbcc7a8d2c336e771af098d108b
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-04 09:35:40 -0400

    first listner

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
-
-let http = require('http')
-let fs = require('fs')
-let url = require('url')
-
-let server = http.createServer()
-server.on('request', (request, response) => {
-    let query = url.parse(request.url, true).query
-    let name = (query.name === undefined) ? 'Anonyme' : query.name
-    fs.readFile('index.html', 'utf-8', (err, data) => {
-        if (err) {
-            response.writeHead(404)
-            response.end("file not found")
-        } else {
-            response.writeHead(200, {'Content-type': 'text/html;'})
-            data = data.replace('{{ name }}', name)
-            response.end(data)
-        }
-    })
+const EventEmitter = require('events')
+let myListner = new EventEmitter()
+myListner.on('jump', function (a, b) {
+    console.log('I jumped', a, b)
 })
-server.listen(80)
+
+myListner.emit('jump', 10, 20)
+myListner.emit('jump')
+myListner.emit('jump', 12)
