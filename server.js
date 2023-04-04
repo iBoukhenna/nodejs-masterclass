884ddd16c503de44f573d050caaf29c7f0dfade9
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-04 09:32:23 -0400

    param to html page

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 
 let http = require('http')
+let fs = require('fs')
 let url = require('url')
 
 let server = http.createServer()
 server.on('request', (request, response) => {
-    response.writeHead(200)
     let query = url.parse(request.url, true).query
-    if (query.name === undefined) {
-        response.write('Hello Anonyme')
-    } else {
-        response.write('Hello ' + query.name)
-    }
-    response.end()
+    let name = (query.name === undefined) ? 'Anonyme' : query.name
+    fs.readFile('index.html', 'utf-8', (err, data) => {
+        if (err) {
+            response.writeHead(404)
+            response.end("file not found")
+        } else {
+            response.writeHead(200, {'Content-type': 'text/html;'})
+            data = data.replace('{{ name }}', name)
+            response.end(data)
+        }
+    })
 })
 server.listen(80)
