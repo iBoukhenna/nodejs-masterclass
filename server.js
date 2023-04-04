9a4314b2885a6e3b6aeeccafb33dcc7d0f4bb7c3
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-04 09:17:18 -0400

    replace response with html page

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 
-var http = require('http')
+let http = require('http')
+let fs = require('fs')
 
-let server = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
-    response.end('Hello')
-}).listen(80)
+let server = http.createServer()
+server.on('request', (request, response) => {
+    fs.readFile('index.html', (err, data) => {
+        if (err) throw err
+        response.writeHead(200, {'Content-type': 'text/html;'})
+        response.end(data)
+    })
+})
+server.listen(80)
