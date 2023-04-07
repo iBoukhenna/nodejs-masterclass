1d7592e10924ac5112a76d4a99624247343bc1eb
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-07 09:54:57 -0400

    use pipe to write from read

--- copy.js
+++ copy.js
@@ -6,14 +6,17 @@ fs.stat(file, (err, stat) => {
     let total = stat.size
     let progress = 0
     let read = fs.createReadStream(file)
+    let write = fs.createWriteStream('copy.mp4')
 
     read.on('data', (chunk) => {
         progress += chunk.length
         console.log('I read ' + Math.round(100*progress/total) + '%')
     })
     
-    read.on('end', () => {
-        console.log('I finished read')
+    read.pipe(write)
+
+    write.on('finish', () => {
+        console.log('the file is copied')
     })
     
 })
