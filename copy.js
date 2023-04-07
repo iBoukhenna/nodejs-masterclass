63ec5865903ceac353a823ea4fc87a03ce259d12
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-07 09:56:02 -0400

    multi write

--- copy.js
+++ copy.js
@@ -7,6 +7,7 @@ fs.stat(file, (err, stat) => {
     let progress = 0
     let read = fs.createReadStream(file)
     let write = fs.createWriteStream('copy.mp4')
+    let write2 = fs.createWriteStream('copy2.mp4')
 
     read.on('data', (chunk) => {
         progress += chunk.length
@@ -14,6 +15,7 @@ fs.stat(file, (err, stat) => {
     })
     
     read.pipe(write)
+    read.pipe(write2)
 
     write.on('finish', () => {
         console.log('the file is copied')
