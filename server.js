2da09ca4baaebe1a979b5c4854c6a186a75eb3c5
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-08 10:23:46 -0400

    add css file with express static

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 
-let app = require('express')()
+let express = require('express')
+let app = express()
 
 app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
 
 app.get('/', function (request, response) {
     response.render('pages/index', {test: 'Hello'})
