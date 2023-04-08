72694bb9d8c7d37defaabc36fb4ef85a806e098a
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-08 12:23:25 -0400

    add moment

--- models/message.js
+++ models/message.js
@@ -1,7 +1,25 @@
 
 let connection = require('../config/db')
+let moment = require('moment')
 
 class Message {
+
+    constructor (row) {
+        this.row = row
+    }
+
+    get id () {
+        return this.row.id
+    }
+
+    get content () {
+        return this.row.content
+    }
+
+    get created_at () {
+        return moment(this.row.created_at)
+    }
+
     static create (content, callback) {
         connection.query('INSERT INTO messages (content, created_at) VALUE (?, ?)', [content, new Date()], (err, result) => {
             if (err) throw err           
@@ -12,7 +30,7 @@ class Message {
     static all (callback) {
         connection.query('SELECT * FROM messages', (err, rows) => {
             if (err) throw err           
-            callback(rows)
+            callback(rows.map((row) => new Message(row)))
         })
     }
 }
