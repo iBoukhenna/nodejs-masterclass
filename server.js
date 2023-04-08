6512b85c164bc3e670e514ad68a400839ecbc65b
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-08 10:25:11 -0400

    add path assets

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let app = express()
 
 app.set('view engine', 'ejs')
 
-app.use(express.static('public'))
+app.use('/assets', express.static('public'))
 
 app.get('/', function (request, response) {
     response.render('pages/index', {test: 'Hello'})
