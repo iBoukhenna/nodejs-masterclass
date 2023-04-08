52fc9a520f56e02f01e3e1c91de6d99b35a6185e
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-08 08:49:56 -0400

    module function

--- server.js
+++ server.js
@@ -1,22 +1,4 @@
-const EventEmitter = require('events')
 
-let http = require('http')
+let demo = require('./hello')
 
-let App = {
-    start: function (port) {
-        let myEmitter = new EventEmitter()
-        let server = http.createServer((request, response) => {
-            response.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
-            if (request.url === '/') {
-                myEmitter.emit('root', response)
-            }
-            response.end()
-        }).listen(port)
-        return myEmitter
-    }
-}
-
-let app = App.start(80)
-app.on('root', function (response) {
-    response.write('I\'m in root')
-})
+demo()
