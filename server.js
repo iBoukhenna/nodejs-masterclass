d5828930cda01ebcc7f75806bf2c90f5dce88191
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-08 10:51:11 -0400

    first form with message error empty

--- server.js
+++ server.js
@@ -2,12 +2,27 @@
 let express = require('express')
 let app = express()
 
+let bodyParser = require('body-parser')
+
+// Template
 app.set('view engine', 'ejs')
 
+// Middleware
 app.use('/assets', express.static('public'))
 
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+
+// Routes
 app.get('/', function (request, response) {
     response.render('pages/index', {test: 'Hello'})
+})
+
+app.post('/', (request, response) => {
+    if (request.body.message === undefined || request.body.message === '') {
+        response.render('pages/index', {error: 'the message is empty'})
+    }
 })
 
 app.listen(80)
