1a78ec53a8749a66c38124e81a44c79dc8047596
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-08 11:28:40 -0400

    use session and redirect

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let express = require('express')
 let app = express()
 
 let bodyParser = require('body-parser')
+let session = require('express-session')
 
 // Template
 app.set('view engine', 'ejs')
@@ -12,16 +13,26 @@ app.use('/assets', express.static('public'))
 
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
-
+app.use(session({
+    secret: 'azertyuiio',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}
+}))
 
 // Routes
 app.get('/', function (request, response) {
-    response.render('pages/index', {test: 'Hello'})
+    if (request.session.error) {
+        response.locals.error = request.session.error
+        request.session.error = undefined
+    }
+    response.render('pages/index')
 })
 
 app.post('/', (request, response) => {
     if (request.body.message === undefined || request.body.message === '') {
-        response.render('pages/index', {error: 'the message is empty'})
+        request.session.error = 'the message is empty'
+        response.redirect('/')
     }
 })
 
